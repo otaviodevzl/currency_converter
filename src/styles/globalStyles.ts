45f02df5b6f5abc713b24f117abdf2ec27626484
Otávio Lima <otavio-lima-co@gmail.com>
2024-10-27 14:31:13 -0300

    first commit

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Open Sans", sans-serif;
+    }
+
+    body {
+        background-color: #639FA6;
+    }
+`
